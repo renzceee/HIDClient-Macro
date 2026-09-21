223f35e7d22ca57c14225f92138cc0d8d0d37dec
Laurence Escalona <dragonpussie@gmail.com>
2026-09-21 11:37:26 +0800

    fix(build): remove hardcoded java.home, detect JBR in gradlew.bat, and chmod gradlew

--- gradlew.bat
+++ gradlew.bat
@@ -41,6 +41,12 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
+if exist "C:\Program Files\Android\Android Studio\jbr\bin\java.exe" (
+    set "JAVA_HOME=C:\Program Files\Android\Android Studio\jbr"
+    set "JAVA_EXE=C:\Program Files\Android\Android Studio\jbr\bin\java.exe"
+    goto execute
+)
+
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
